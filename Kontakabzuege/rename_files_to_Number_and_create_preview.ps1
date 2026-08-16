f6494410ed4677eb203d283d1571eae7ec5856ed
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2026-08-16 18:52:36 +0200

    Add script to rename image files with creation date
    This script renames image files in a specified folder based on their creation date and ensures the base name is a four-digit number.

---
+++ Kontakabzuege/rename_files_to_Number_and_create_preview.ps1
@@ -0,0 +1,14 @@
+$Ordner = "C:\Pfad\Zu\Deinem\Bilderordner"
+
+Get-ChildItem -LiteralPath $Ordner -File |
+    Where-Object {
+        $_.Extension -match '^\.(jpg|jpeg|png)$' -and
+        $_.BaseName -match '^\d{4}$'
+    } |
+    ForEach-Object {
+
+        $Datum = $_.CreationTime.ToString("dd.MM.yyyy")
+        $NeuerName = "{0}-({1}){2}" -f $_.BaseName, $Datum, $_.Extension
+
+        Rename-Item -LiteralPath $_.FullName -NewName $NeuerName -WhatIf
+    }
